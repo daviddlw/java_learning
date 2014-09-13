7b5f5c7e7f0ea8d00b01b30ae3557b88d91127c7
daviddlw <davidjefiny@gmail.com>
2014-09-14 01:10:27 +0800

    反射第二讲

--- src/main/java/com/david/reflect/ReflectionDemo.java
+++ src/main/java/com/david/reflect/ReflectionDemo.java
@@ -1,14 +1,119 @@
 package com.david.reflect;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+import org.apache.commons.lang3.ClassUtils.Interfaces;
 
 public class ReflectionDemo
 {
+	private static Method smokingMethod;
+
 	public static void main(String[] args)
 	{
 		// generateObjWithReflect();
-//		constructorDemo();
-		dynamicInstance();
+		// constructorDemo();
+		// dynamicInstance();
+		// fieldDemo();
+		// superClassDemo();
+		arrayDemo();
+	}
+
+	/**
+	 * Arrays工具类用法
+	 */
+	private static void arrayDemo()
+	{
+		User[] users = new User[] { new User("张三", "123456", 25), new User("李四", "123456", 26), new User("王五", "123456", 27) };
+
+		System.out.println("对象数组的长度：" + Array.getLength(users));
+		Class<?> c = users.getClass();
+		Class<?> c2 = User.class;
+		System.out.println("是否为数组类型：" + c.isArray());
+		System.out.println("是否为数组类型：" + c2.isArray());
+		
+		for (int i = 0; i < Array.getLength(users); i++)
+		{
+			System.out.println(Array.get(users, i));
+		}
+	}
+
+	private static void superClassDemo()
+	{
+		Class<?> c = User.class;
+		try
+		{
+			User u = (User) c.newInstance();
+			Class<?> sc = c.getSuperclass();
+			System.err.println(sc);
+		} catch (InstantiationException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * field反射
+	 */
+	private static void fieldDemo()
+	{
+		Class<?> c = User.class;
+
+		System.out.println("==========所有成员变量==========");
+		Field[] fields = c.getDeclaredFields();
+		for (Field field : fields)
+		{
+			System.err.println(field.toGenericString());
+		}
+
+		System.out.println();
+		System.out.println("==========所有成员方法===============");
+		Method[] methods = c.getDeclaredMethods();
+		for (Method method : methods)
+		{
+			System.err.println(method);
+		}
+
+		System.out.println();
+		System.err.println("===========所有公共方法================");
+		Method[] otherMethods = c.getMethods();
+		for (Method method : otherMethods)
+		{
+			System.out.println(method);
+		}
+
+		System.err.println("=============反射调用公共方法==========");
+		try
+		{
+			Method sayHelloMethod = c.getMethod("showMessage", new Class[] { String.class });
+			sayHelloMethod.invoke(new User(), new Object[] { "你好" });
+
+			User u = (User) c.newInstance();
+			smokingMethod = c.getDeclaredMethod("smoking", new Class[] {});
+			smokingMethod.setAccessible(true);
+			smokingMethod.invoke(u, null);
+
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		System.err.println("=========获取接口信息============");
+		Class<?>[] interfaces = c.getInterfaces();
+		for (Class<?> item : interfaces)
+		{
+			System.out.println(item.toString());
+		}
 	}
 
 	/**
@@ -23,9 +128,10 @@ public class ReflectionDemo
 			Constructor<?> constructor = c.getConstructor(new Class[] { String.class, String.class, int.class });
 			User u = (User) constructor.newInstance(new Object[] { "david.dai", "123456", 27 });
 			System.err.println(u);
-			
+
 		} catch (Exception e)
 		{
+			e.printStackTrace();
 			// TODO: handle exception
 		}
 	}
--- src/main/java/com/david/reflect/User.java
+++ src/main/java/com/david/reflect/User.java
@@ -1,10 +1,17 @@
 package com.david.reflect;
 
-public class User
+import java.io.Serializable;
+
+public class User implements Serializable, Comparable
 {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String name;
 	private String password;
 	private int age;
+	private double salary;
 
 	public User()
 	{
@@ -15,6 +22,7 @@ public class User
 	private User(String name, String password)
 	{
 		super();
+		salary = 0;
 	}
 
 	public User(String name, String password, int age)
@@ -55,10 +63,32 @@ public class User
 		this.age = age;
 	}
 
+	public void sayHello()
+	{
+		System.err.println("你好");
+	}
+
+	private void smoking()
+	{
+		System.err.println("抽烟");
+	}
+
+	public void showMessage(String message)
+	{
+		System.err.println("Message： " + message);
+	}
+
 	@Override
 	public String toString()
 	{
 		return "User [name=" + name + ", password=" + password + ", age=" + age + "]";
+	}
+
+	@Override
+	public int compareTo(Object o)
+	{
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
