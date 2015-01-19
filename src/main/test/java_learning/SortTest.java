cf9d7169ff5ab59fc8df6fc1e66afba28d3377ac
daviddlw <274714299@qq.com>
2015-01-19 18:14:42 +0800

    check in~

---
+++ src/main/test/java_learning/SortTest.java
@@ -0,0 +1,22 @@
+package java_learning;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SortTest
+{
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void showMessage()
+	{
+		System.out.println(1);
+	}
+
+}
